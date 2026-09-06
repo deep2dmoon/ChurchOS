a9e35875fd17f6cd7e92427573622c36f1e87474
cosmosayo <alfredchrisayo@gmail.com>
2026-09-06 02:19:34 -0700

    fix: dockerfile

--- obj/Debug/net10.0/htmos.AssemblyInfo.cs
+++ obj/Debug/net10.0/htmos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("htmos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3261f8fe1315f05bf486509126615ff8986c529e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b51b8388e38f4822477987f2cbaf3ee8c4e9cee")]
 [assembly: System.Reflection.AssemblyProductAttribute("htmos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("htmos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
